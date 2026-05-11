89f4666c42cd71294052ecfb453dde73f9923cdd
ilailsonrocha <ilailsonrocha@iec.gov.br>
2026-05-11 09:12:39 -0300

    refactor: back: simplificar Dockerfile e wsgi.py, removendo código desnecessário

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,29 +1,3 @@
-from flask import Flask
-from flask_cors import CORS
-from extensions import db
+from app import create_app
 
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    db.init_app(app)
-    CORS(app)
-
-    from models import cliente, emprestimo, pagamento
-    from routes.cliente_routes import cliente_bp
-    from routes.emprestimo_routes import emprestimo_bp
-    from routes.pagamento_routes import pagamento_bp
-    
-    app.register_blueprint(cliente_bp)
-    app.register_blueprint(emprestimo_bp)
-    app.register_blueprint(pagamento_bp)
-
-    with app.app_context():
-        db.create_all()
-
-    return app
-
-
-# Para Gunicorn
 app = create_app()
